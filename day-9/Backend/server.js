e9d9c3b5dbdc8a50d25e9648e82deaf35c55ed49
Akshvt <128501282+Akshvt@users.noreply.github.com>
2026-01-31 21:43:22 +0530

    Day-9 Implemented

---
+++ day-9/Backend/server.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const app = require('./src/app')
+const ConnectToDb = require('./src/config/database')
+
+ConnectToDb()
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000")
+})
---
+++ day-9/Backend/src/app.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const noteModel = require("./models/note.model")
+
+const app = express()
+
+app.use(express.json())
+
+app.post('/api/notes', async(req,res)=>{
+    const{title,description} = req.body
+
+    await noteModel.create({
+        title,description
+    })
+
+    res.status(201).json({
+        message:"Notes created Successfully"
+    })
+})
+
+app.get('/api/notes', async(req,res)=>{
+    const{title,description} = req.body
+
+    const notes = await noteModel.find() //this finds all data in ur collection and returns an array of data
+
+    res.status(200).json({
+        message:"Here are your Notes",
+        notes
+    })
+})
+
+app.delete('/api/notes/:id', async(req,res)=>{
+    const id = req.params.id
+    await noteModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message:"Note deleted Successfully"
+    })
+})
+
+
+app.patch('/api/notes/:id',async (req,res)=>{
+    const id = req.params.id
+    const{description} = req.body
+
+   await noteModel.findByIdAndUpdate(id,{description})
+
+   res.status(200).json({
+    message:"Updated your notes!"
+   })
+})
+
+
+module.exports = app
